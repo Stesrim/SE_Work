26b91328fa89bf7174bde7ba54e97bbd9626e222
林彥呈 <cei111018@nptu.edu.tw>
2024-11-13 15:35:39 +0800

    修好頁面問題

--- demo/src/main/java/mazemaker/Page.java
+++ demo/src/main/java/mazemaker/Page.java
@@ -7,13 +7,11 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
  
 public class Page extends JPanel{  
+    private boolean[] exists = {false, false, false, false, false};
     private JTabbedPane tabbedPane;
-    private int tabCount = 1;
  
     public Page() {
-        // 初始化 JTabbedPane
-        tabbedPane = new JTabbedPane(); 
-        // 設定面板佈局並添加元件
+        tabbedPane = new JTabbedPane();
         setLayout(new BorderLayout());
         add(tabbedPane, BorderLayout.CENTER);
     }
@@ -21,17 +19,28 @@ public class Page extends JPanel{
     public void removeCurrentTab() {
         int index = tabbedPane.getSelectedIndex();
         if (index >= 0) {
+            // 抓取目前頁面的名稱
+            String title = tabbedPane.getTitleAt(index);
+            // \\D為所有非數字 取得該頁數的數字
+            String numberPart = title.replaceAll("\\D+", "");
+            exists[Integer.parseInt(numberPart) - 1] = false;
             tabbedPane.removeTabAt(index);
         }
     }
     //新增頁面
     public void addNewTab() {
-        JPanel panel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("這是頁面 " + tabCount); 
-        // 添加元件到頁面
-        panel.add(label, BorderLayout.CENTER);
-        // 將新頁面添加到 JTabbedPane
-        tabbedPane.addTab("頁面 " + tabCount, panel);
-        tabCount++; // 更新頁面計數
+        for (int i = 0 ; i <= exists.length; i++){
+            if (exists[i] == false){
+                JPanel panel = new JPanel(new BorderLayout());
+                JLabel label = new JLabel("這是頁面 " + (i +1)); 
+                // 添加元件到頁面
+                panel.add(label, BorderLayout.CENTER);
+                // 將新頁面添加到 JTabbedPane
+                tabbedPane.addTab("頁面 " + (i +1), panel);
+                exists[i] = true;
+                break;
+               
+            }
+        }
     }
 }
