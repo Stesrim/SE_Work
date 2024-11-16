1fd3fe41671aade300429d2149c9bfac57c5589d
cbb111133 <cbb111133@nptu.edu.tw>
2024-11-16 15:55:37 +0800

    物件UI檔案分裝

--- demo/src/main/java/mazemaker/makemap.java
+++ demo/src/main/java/mazemaker/makemap.java
@@ -10,10 +10,12 @@ public class makemap extends JFrame {
 	public mazedesignwin designWin;
 	public mazedesignmenu dm;
 	public Page tabpane;
+	public toolbar tb;
    	makemap(){
     	tabpane = new Page();
 		designWin = new mazedesignwin(this); //創建設計端頁面
 		dm = new mazedesignmenu(this, tabpane); //創建設計端菜單&工具列
+		tb = new toolbar(this);
 		designWin.add(tabpane, BorderLayout.CENTER);
    	}
 }
